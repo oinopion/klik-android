edc774eaf66f3c49db4aa37079a2d732389bdb7f
Tomek Paczkowski <tomek@hauru.eu>
2017-04-14 11:07:31 +0100

    Added button that increments

--- app/src/main/java/eu/hauru/klik/CounterActivity.java
+++ app/src/main/java/eu/hauru/klik/CounterActivity.java
@@ -2,12 +2,24 @@ package eu.hauru.klik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
+    int counterValue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+    }
+
+    public void incrementCounter(View view) {
+        counterValue += 1;
+        String paddedValue = String.format("%05d", counterValue);
+
+        TextView counterValueView = (TextView) findViewById(R.id.counterValue);
+        counterValueView.setText(paddedValue);
+
     }
 }
