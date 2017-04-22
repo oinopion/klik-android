2f3f79502cf59f3bf829a9855df52bba321cbcca
Tomek Paczkowski <tomek@hauru.eu>
2017-04-22 21:43:04 +0100

    Add name to CounterState

--- app/src/main/java/eu/hauru/klik/CounterActivity.java
+++ app/src/main/java/eu/hauru/klik/CounterActivity.java
@@ -16,9 +16,9 @@ import android.widget.TextView;
 import java.util.Locale;
 
 public class CounterActivity extends AppCompatActivity {
-    final static String STATE_PREFERENCES = "state";
 
     private CounterState state;
+    private TextView counterNameView;
     private TextView counterValueView;
 
     @Override
@@ -26,8 +26,15 @@ public class CounterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
         counterValueView = (TextView) findViewById(R.id.counterValue);
+        counterNameView = (TextView) findViewById(R.id.counterName);
 
         restoreState();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        renderCounterName();
         renderCounterValue();
     }
 
@@ -76,12 +83,16 @@ public class CounterActivity extends AppCompatActivity {
     }
 
     private void restoreState() {
-        SharedPreferences statePreferences = getSharedPreferences(STATE_PREFERENCES, MODE_PRIVATE);
-        state = new CounterState(statePreferences);
+        state = new CounterState(getApplicationContext());
     }
 
     private void renderCounterValue() {
         String paddedValue = String.format(Locale.ENGLISH, "%05d", state.getValue());
         counterValueView.setText(paddedValue);
     }
+
+    private void renderCounterName() {
+        counterNameView.setText(state.getName());
+    }
+
 }
--- app/src/main/java/eu/hauru/klik/CounterState.java
+++ app/src/main/java/eu/hauru/klik/CounterState.java
@@ -1,22 +1,38 @@
 package eu.hauru.klik;
 
 
+import android.content.Context;
 import android.content.SharedPreferences;
 
+
 class CounterState {
+    final static String STATE_PREFERENCES = "state";
     final static String COUNTER_VALUE_KEY = "counterValue";
+    final static String COUNTER_NAME_KEY = "counterName";
+
 
     private int counterValue;
-    private final SharedPreferences preferences;
+    private String counterName;
 
-    public CounterState(SharedPreferences preferences) {
+    private final SharedPreferences preferences;
+    private final int initialCounterValue = 0;
+    private final String defaultCounterName;
+
+    public CounterState(Context context) {
         super();
-        this.preferences = preferences;
+        this.preferences = context.getSharedPreferences(STATE_PREFERENCES, Context.MODE_PRIVATE);
+        this.defaultCounterName = context.getString(R.string.default_counter_name);
+
         this.counterValue = fetchCounterValue();
+        this.counterName = fetchCounterName();
     }
 
     public int getValue() {
         return counterValue;
+    }
+
+    public String getName() {
+        return counterName;
     }
 
     public void incrementValue() {
@@ -29,13 +45,28 @@ class CounterState {
         saveCounterValue();
     }
 
+    public void changeName(String name) {
+        this.counterName = name;
+        saveCounterName();
+    }
+
     private int fetchCounterValue() {
-        return preferences.getInt(COUNTER_VALUE_KEY, 0);
+        return preferences.getInt(COUNTER_VALUE_KEY, initialCounterValue);
+    }
+
+    private String fetchCounterName() {
+        return preferences.getString(COUNTER_NAME_KEY, defaultCounterName);
     }
 
     private void saveCounterValue() {
         SharedPreferences.Editor editor = preferences.edit();
         editor.putInt(COUNTER_VALUE_KEY, counterValue);
+        editor.apply();
+    }
+
+    private void saveCounterName() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(COUNTER_NAME_KEY, counterName);
         editor.apply();
     }
 }
