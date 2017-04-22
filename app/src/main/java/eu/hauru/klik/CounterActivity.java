b3d904ad671135b712b4ece3c6475fab6cef2d02
Tomek Paczkowski <tomek@hauru.eu>
2017-04-22 20:42:15 +0100

    Add CounterEditActivity

--- app/src/main/java/eu/hauru/klik/CounterActivity.java
+++ app/src/main/java/eu/hauru/klik/CounterActivity.java
@@ -1,14 +1,19 @@
 package eu.hauru.klik;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.Locale;
 
-public class CounterActivity extends Activity {
+public class CounterActivity extends AppCompatActivity {
     final static String STATE_PREFERENCES = "state";
 
     private CounterState state;
@@ -24,9 +29,25 @@ public class CounterActivity extends Activity {
         renderCounterValue();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_counter_menu, menu);
+        return true;
+    }
+
     public void incrementCounter(View view) {
         state.incrementValue();
         renderCounterValue();
+    }
+
+    public void resetCounter(MenuItem item) {
+
+    }
+
+    public void switchToEditActivity(MenuItem item) {
+        Intent intent = new Intent(this, CounterEditActivity.class);
+        startActivity(intent);
     }
 
     private void restoreState() {
---
+++ app/src/main/java/eu/hauru/klik/CounterEditActivity.java
@@ -0,0 +1,18 @@
+package eu.hauru.klik;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CounterEditActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter_edit);
+    }
+
+
+}
+
