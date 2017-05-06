10cff6cc9862dc507d10eab19a211b6c851a43c2
Tomek Paczkowski <tomek@hauru.eu>
2017-05-06 20:48:42 +0100

    Add new data layer

---
+++ app/src/main/java/eu/hauru/klik/Counter.java
@@ -0,0 +1,44 @@
+package eu.hauru.klik;
+
+
+import android.text.TextUtils;
+
+import java.util.Date;
+import java.util.UUID;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class Counter extends RealmObject {
+    @PrimaryKey
+    @Required
+    private String id;
+    private Date createdAt = new Date();
+    private int value = 0;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean hasName() {
+        return !TextUtils.isEmpty(name);
+    }
+
+    void incrementValue() {
+        value += 1;
+    }
+
+    void setName(String name) {
+        this.name = name.trim();
+    }
+}
